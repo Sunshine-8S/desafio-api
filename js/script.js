658be99ee9d1002f14dc295de18660d5456029bd
Sol Gonzalez <solvivianags@gmail.com>
2025-05-25 21:55:12 -0500

    CSS y boton de siguiente usuario

--- js/script.js
+++ js/script.js
@@ -5,25 +5,25 @@ const $listaUsuario = document.getElementById("listaUsuario");
 const $fotoUsuario = document.getElementById("fotoUsuario");
 const $direccion = document.getElementById("direccion");
 const $credenciales = document.getElementById("credenciales");
+const $btnSiguiente = document.getElementById("btnSiguiente");
 
-fetch("https://randomuser.me/api")
+$btnSiguiente.addEventListener("click", usuario);
+
+function usuario() {
+    fetch("https://randomuser.me/api")
     .then((response)=>response.json())
     .then((user)=> {
 
         const usuario = user.results[0];
-        console.log(`User =`, user);
 
         $titulo.textContent = `${usuario.name.first} ${usuario.name.last}`;
 
-        $fotoUsuario.innerHTML = `<img src="${usuario.picture.large}" alt="Foto de usuario">`;
+        $fotoUsuario.innerHTML = `<img class="foto-usuario" src="${usuario.picture.large}" alt="Foto de usuario">`;
 
         $listaUsuario.innerHTML = `
-            <li class="subtitulo">Edad:</li>
-            <li>${usuario.dob.age}</li>
-            <li class="subtitulo">Celular:</li>
-            <li>${usuario.cell}</li>
-            <li class="subtitulo">Correo:</li>
-            <li>${usuario.email}</li>
+            <li>Edad: ${usuario.dob.age}</li>
+            <li>Celular: ${usuario.cell}</li>
+            <li>Correo: ${usuario.email}</li>
         `;
 
         $direccion.innerHTML = `<p>${usuario.location.street.name} ${usuario.location.street.number}, ${usuario.location.city} ${usuario.location.state}. ${usuario.location.country}</p>`;
@@ -34,9 +34,9 @@ fetch("https://randomuser.me/api")
             <p>Fecha de registro: ${usuario.registered.date}</p>
         `;
 
-    }
-)
-.catch(()=> {
-    alert(`Ha oocurrido un error, por favor intenta más tarde`);
-});
+    })
+    .catch(()=> {
+        alert(`Ha oocurrido un error, por favor intenta más tarde`);
+    });
+}
 
